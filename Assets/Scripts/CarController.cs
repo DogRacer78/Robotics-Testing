70f21add1659e2e2ffa6572d264d566e31be72c5
DogRacer-Laptop <doggieracer@hotmail.com>
2022-10-05 18:35:05 +0100

    Added rpm counter

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class CarController : MonoBehaviour
     [SerializeField] private List<Axle> axles;
     [SerializeField] private float maxSteeringAngle = 20;
     [SerializeField] private float maxMotorTorque = 400;
+    [SerializeField] private GameObject rpmCounter;
 
     private float steering;
     private float motorTorque;
@@ -18,15 +20,14 @@ public class CarController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Debug.Log(rpmCounter.GetComponent<TMPro.TextMeshProUGUI>().text);
     }
 
     // Update is called once per frame
     void Update()
     {
         //get the input
-        steering = Input.GetAxis("Horizontal") * maxSteeringAngle;
-        motorTorque = Input.GetAxis("Vertical") * maxMotorTorque;
+        
 
         if (Input.GetKey(KeyCode.Space))
             brake = true;
@@ -39,6 +40,10 @@ public class CarController : MonoBehaviour
         //set the steering angle of the front wheels
         foreach (Axle axle in axles)
         {
+            float 
+            steering = Input.GetAxis("Horizontal") * maxSteeringAngle;
+            motorTorque = Input.GetAxis("Vertical") * maxMotorTorque;
+
             if (axle.steering)
             {
                 axle.left.steerAngle = steering;
@@ -47,16 +52,18 @@ public class CarController : MonoBehaviour
 
             if (brake)
             {
-                Debug.Log("Braking");
-                axle.left.brakeTorque = 100;
-                axle.right.brakeTorque = 100;
-                //continue;
+                //Debug.Log("Braking");
+                //axle.left.brakeTorque = 100;
+                //axle.right.brakeTorque = 100;
+                ////continue;
+                axle.left.wheelDampingRate = 100;
+                axle.left.wheelDampingRate = 100;
             }
-            //else
-            //{
-            //    axle.left.brakeTorque = 0;
-            //    axle.right.brakeTorque = 0;
-            //}
+            else
+            {
+                //axle.left.brakeTorque = 0;
+                //axle.right.brakeTorque = 0;
+            }
 
             if (axle.motor)
             {
@@ -64,5 +71,8 @@ public class CarController : MonoBehaviour
                 axle.right.motorTorque = motorTorque;
             }
         }
+
+        //can use wheel damping as brakes
+        rpmCounter.GetComponent<TMPro.TextMeshProUGUI>().text = "RPM: " + Math.Round(axles[1].left.rpm, 2).ToString();
     }
 }
