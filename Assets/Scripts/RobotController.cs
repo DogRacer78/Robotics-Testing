373f50ee769aaedf238d6768dd16cc151eb169d7
DogRacer <doggieracer@hotmail.com>
2022-10-16 23:18:09 +0100

    Removed thor robot as too large

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
+public enum RotationDir { None = 0, Pos = 1, Neg = -1}
 public class RobotController : MonoBehaviour
 {
+
     private ArticulationBody articulation;
+    [SerializeField] private float speed = 300.0f;
+
+    private float horizontal = 0.0f;
+    private RotationDir rotationDir = RotationDir.None;
     private void Start()
     {
        articulation = GetComponent<ArticulationBody>();
@@ -12,8 +19,23 @@ public class RobotController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        
+        if (horizontal == 0.0f)
+        {
+            rotationDir = RotationDir.None; 
+        }
+        else if (horizontal > 0.0f)
+        {
+            rotationDir = RotationDir.Pos;
+        }
+        else if (horizontal < 0.0f)
+        {
+            rotationDir = RotationDir.Neg;
+        }
+
         float currentRotation = CurrentPrimaryAxisRotation();
-        float rotationGoal = currentRotation + (1000.0f * Time.fixedDeltaTime);
+        float rotationGoal = currentRotation + (speed * Time.fixedDeltaTime) * (int)rotationDir;
         RotateTo(rotationGoal);
 
         Debug.Log(rotationGoal);
