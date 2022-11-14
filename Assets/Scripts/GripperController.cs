a6a04784f49db8ae958cde3ff9c58f414b5ae828
DogRacer-Laptop <doggieracer@hotmail.com>
2022-11-14 10:41:01 +0000

    Robot Arm Working

---
+++ Assets/Scripts/GripperController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripperController : MonoBehaviour
+{
+
+    private ArticulationBody art;
+    private float speed = 100.0f;
+    public bool invert = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        art = GetComponent<ArticulationBody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            float currentRotation = CurrentPrimaryAxisRotation(art);
+            float rotationGoal = 0.0f;
+            if (invert)
+            {
+                rotationGoal = currentRotation + (speed * Time.fixedDeltaTime) * -1;
+            }
+            else
+            {
+                rotationGoal = currentRotation + (speed * Time.fixedDeltaTime) * 1;
+            }
+            
+            RotateTo(rotationGoal, art);
+        }
+        else if (Input.GetKey(KeyCode.J))
+        {
+            float currentRotation = CurrentPrimaryAxisRotation(art);
+            float rotationGoal = 0.0f;
+            if (invert)
+            {
+                rotationGoal = currentRotation + (speed * Time.fixedDeltaTime) * 1;
+            }
+            else
+            {
+                rotationGoal = currentRotation + (speed * Time.fixedDeltaTime) * -1;
+            }
+            RotateTo(rotationGoal, art);
+        }
+        
+    }
+
+    float CurrentPrimaryAxisRotation(ArticulationBody articulation)
+    {
+        float rads = articulation.jointPosition[0];
+        float currentRotation = Mathf.Rad2Deg * rads;
+        return currentRotation;
+    }
+
+    void RotateTo(float primaryAxisRotation, ArticulationBody articulation)
+    {
+        var drive = articulation.xDrive;
+        drive.target = primaryAxisRotation;
+        Debug.Log("xDrive " + articulation.xDrive.target);
+        articulation.xDrive = drive;
+    }
+}
---
+++ Assets/Scripts/RotateGripper.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGripper : MonoBehaviour
+{
+    public GameObject movingArm;
+    private Transform movingArmTransform = null;
+    private ArticulationBody art;
+    void Start()
+    {
+        movingArmTransform = movingArm.GetComponent<Transform>();
+        art = this.GetComponent<ArticulationBody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // 
+        float rots = -(movingArmTransform.rotation.eulerAngles.x - 270.0f);
+        Debug.Log("Rots: " + (movingArmTransform.rotation.eulerAngles.x - 270.0f));
+        RotateTo(-movingArm.GetComponent<ArticulationBody>().xDrive.target, art);
+    }
+
+    public void RotateTo(float rotationGoal, ArticulationBody art)
+    {
+        var drive = art.xDrive;
+        drive.target = rotationGoal;
+        art.xDrive = drive;
+    }
+}
